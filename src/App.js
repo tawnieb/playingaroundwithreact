5ba342814493b6a5cfb54c2da4db49704bb6c645
Tawnie Bavaro <tawnie.bavaro@concreteplatform.com>
2019-02-15 17:53:12 +0000

    added css

--- src/App.js
+++ src/App.js
@@ -53,13 +53,15 @@ onSearchChange = (event) => {
     //destructuring
     const { searchTerm, list } = this.state;
     return  (
-      <div className="App">
+      <div className="page">
+        <div className="interactions">
         <Search 
           value={searchTerm}
           onChange={this.onSearchChange}
         >
           Search
         </Search>
+        </div>
         <Table 
           list={list}
           pattern={searchTerm}
@@ -70,10 +72,7 @@ onSearchChange = (event) => {
   }
 }
 
-  class Search extends Component {
-    render() {
-      const { value, onChange, children } = this.props;
-      return (
+  const Search = ({ value, onChange, children }) =>
         <form>
           { children } <input 
             type="text"
@@ -81,36 +80,42 @@ onSearchChange = (event) => {
             onChange={onChange}
           />
         </form>
-      );
-    }
-  }
 
-  class Table extends Component {
-    render() {
-      const { list, pattern, onDismiss } = this.props;
-      return (
-        <div>
+  const Table = ({ list, pattern, onDismiss }) => 
+        <div className="table">
           {list.filter(isSearched(pattern)).map(item => 
-            <div key={item.objectID}>
-              <span>
+            <div key={item.objectID} className="table-row">
+              <span style={{ width: '40%' }}>
                 <a href={item.url}>{item.title}</a>
               </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
+              <span style={{ width: '30% '}}>
+                {item.author}
+              </span>
+              <span style={{ width: '10%' }}>
+                {item.num_comments}
+              </span>
+              <span style={{ width: '10%' }}>
+                {item.points}
+              </span>
               <span>
-                <button
-                  onClick={() => onDismiss(item.objectID)}
-                  type="button"
-                >
-                  Dismiss
-                </button>
+              <Button 
+                onClick={() => onDismiss(item.objectID)}
+                className="button-inline"
+              > 
+                Dismiss
+              </Button>
               </span>
             </div>
           )}
         </div>
-      );
-    }
-  }
 
+const Button = ({ onClick, className = '', children }) => 
+        <button
+          onClick={onClick}
+          className={className}
+          type="button"
+        >
+          {children}
+        </button>
+  
   export default App;
