73aecccf3040d445df5763ca5fd9635f0c8c79cb
Tawnie Bavaro <tawnie.bavaro@concreteplatform.com>
2019-02-18 17:26:14 +0000

    refactored to use API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'http://reactjs.org',
-    author: 'Jordon Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'http://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'http://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 const isSearched = searchTerm => item => 
     item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -30,18 +17,29 @@ constructor(props) {
   // them there, otherwise they would be undefined
   super(props);
   this.state = {
-    list,
-    searchTerm: '',
+    result: null,
+    searchTerm: DEFAULT_QUERY,
   };
+}
+
+setSearchTopStories = (result) => {
+  this.setState({ result });
+}
+
+componentDidMount() {
+  const { searchTerm } = this.state;
+
+fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  .then(response => response.json())
+  .then(result => this.setSearchTopStories(result))
+  .catch(error => error);
 }
 
 //filter function returns new list instead of mutating old one
 onDismiss = (id) => {
-  const updatedList = 
-    this.state.list.filter(
-      item => item.objectID !== id
-    );
-  this.setState({ list: updatedList });
+  const isNotId = item => item.objectID !== id;
+  const updatedHits = this.state.result.hits.filter(isNotId);
+  this.setState({ ... });
 }
 
 onSearchChange = (event) => {
@@ -51,7 +49,9 @@ onSearchChange = (event) => {
   //use map to render a list of items as HTML elements
   render() {
     //destructuring
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    if(!result) { return null; }
+
     return  (
       <div className="page">
         <div className="interactions">
@@ -63,7 +63,7 @@ onSearchChange = (event) => {
         </Search>
         </div>
         <Table 
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
@@ -88,7 +88,7 @@ onSearchChange = (event) => {
               <span style={{ width: '40%' }}>
                 <a href={item.url}>{item.title}</a>
               </span>
-              <span style={{ width: '30% '}}>
+              <span style={{ width: '30% ' }}>
                 {item.author}
               </span>
               <span style={{ width: '10%' }}>
