486a8d20c07591543bc63086a4c57c623e2f3b55
Tawnie Bavaro <tawnie.bavaro@concreteplatform.com>
2019-02-19 17:02:28 +0000

    refactored

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'http://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-
-const isSearched = searchTerm => item => 
-    item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
 
@@ -17,29 +17,52 @@ constructor(props) {
   // them there, otherwise they would be undefined
   super(props);
   this.state = {
-    result: null,
+    results: null,
+    searchKey: '',
     searchTerm: DEFAULT_QUERY,
   };
 }
 
 setSearchTopStories = (result) => {
-  this.setState({ result });
+  const { hits, page } = result;
+  const { searchKey, results } = this.state;
+  const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
+  const updatedHits = [...oldHits, ...hits];
+  this.setState({
+    results: { 
+      ...results, 
+      [searchKey]: { hits: updatedHits, page }
+    }
+  });
+}
+
+fetchSearchTopStories = (searchTerm, page = 0) => {
+  fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(error => error)
 }
 
 componentDidMount() {
   const { searchTerm } = this.state;
+  this.setState({ searchKey: searchTerm })
+  this.fetchSearchTopStories(searchTerm);
+}
 
-fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-  .then(response => response.json())
-  .then(result => this.setSearchTopStories(result))
-  .catch(error => error);
+onSearchSubmit = (event) => {
+  const { searchTerm } = this.state;
+  this.setState({ searchKey: searchTerm });
+  this.fetchSearchTopStories(searchTerm);
+  event.preventDefault();
 }
 
 //filter function returns new list instead of mutating old one
 onDismiss = (id) => {
   const isNotId = item => item.objectID !== id;
   const updatedHits = this.state.result.hits.filter(isNotId);
-  this.setState({ ... });
+  this.setState({
+    result: { ...this.state.result, hits: updatedHits }
+  });
 }
 
 onSearchChange = (event) => {
@@ -50,40 +73,50 @@ onSearchChange = (event) => {
   render() {
     //destructuring
     const { searchTerm, result } = this.state;
-    if(!result) { return null; }
-
+    const page = (result && result.page) || 0;
     return  (
       <div className="page">
         <div className="interactions">
         <Search 
           value={searchTerm}
           onChange={this.onSearchChange}
+          onSubmit={this.onSearchSubmit}
         >
           Search
         </Search>
         </div>
-        <Table 
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result &&
+          <Table 
+              list={result.hits}
+              onDismiss={this.onDismiss}
+          />
+        }
+      <div className='interactions'>
+        <Button onClick={() => 
+          this.fetchSearchTopStories(searchTerm, page + 1)}>
+          More
+        </Button>
+      </div>
       </div>
     );  
   }
 }
 
-  const Search = ({ value, onChange, children }) =>
-        <form>
-          { children } <input 
+  const Search = ({ value, onChange, onSubmit, children }) =>
+        <form onSubmit={onSubmit}>
+          <input 
             type="text"
             value={value}
             onChange={onChange}
           />
+          <button type="submit">
+            {children}
+          </button>
         </form>
 
-  const Table = ({ list, pattern, onDismiss }) => 
+  const Table = ({ list, onDismiss }) => 
         <div className="table">
-          {list.filter(isSearched(pattern)).map(item => 
+          {list.map(item => 
             <div key={item.objectID} className="table-row">
               <span style={{ width: '40%' }}>
                 <a href={item.url}>{item.title}</a>
